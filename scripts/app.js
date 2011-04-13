e2ed6ef168198d33a46f331a1887ed481129db7a
ehynds <eric@hynds.net>
2011-04-13 13:45:43 -0400

    preload the grouped view icon

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,28 @@
 (function(){
-    window.app = {};
+	window.app = {};
 
-    $LAB.setGlobalDefaults({
-        AlwaysPreserveOrder:true,
-        BasePath: 'scripts/'
-    });
+	$LAB.setGlobalDefaults({
+		AlwaysPreserveOrder:true,
+		BasePath: 'scripts/'
+	});
 
-    $LAB
-    .script('libs/jquery.min.js')
-    .script('libs/jquery-ui.min.js')
-    .script('libs/underscore.js')
-    .script('libs/backbone.js')
-    .script('libs/backbone.localstorage.js')
-    .script('libs/faker.js')
-    .script('models/contacts.js')
-    .script('views/details.js')
-    .script('views/contacts.js')
-    .script('views/contactslist.js')
-    .script('views/addcontact.js')
-    .script('views/rendermode.js')
-    .script('views/filter.js');
+	$LAB
+	.script('libs/jquery.min.js')
+	.script('libs/jquery-ui.min.js')
+	.script('libs/underscore.js')
+	.script('libs/backbone.js')
+	.script('libs/backbone.localstorage.js')
+	.script('libs/faker.js')
+	.script('models/contacts.js')
+	.script('views/details.js')
+	.script('views/contacts.js')
+	.script('views/contactslist.js')
+	.script('views/addcontact.js')
+	.script('views/rendermode.js')
+	.script('views/filter.js');
+
+	// preload the grouped view icon
+	var img = document.createElement('img');
+	img.src = 'images/view-group.gif';
+
 })();
